98e9665c85a73cc9bd53c3e7e5530f2c86461487
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-05-16 23:48:44 +0530

    Add nanoid dependency and implement URL creation endpoint

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import {nanoid} from 'nanoid';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.listen(3000,()=>{
     console.log('Server is running http://localhost:3000/');
 })
 
 app.get('/api/create',(req,res)=>{
-    res.send("create short URL")
+    // res.send("create short URL");
+    const {url} = req.body;
+    console.log(url);
+    res.send(nanoid(10));
 })
 
 app.get('/', (req, res) => {
